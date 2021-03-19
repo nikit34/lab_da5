6166d5013a443604e560ed31ee4aa43d93995c37
nikit34 <permikov134@mail.ru>
2021-03-19 15:20:41 +0300

    add find method

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 
 using namespace std;
@@ -17,11 +18,35 @@ TSuffixTree::TSuffixTree(string str){
 class TArray{
 public:
     TArray(TSuffixTree tree);
-    std::vector<int> Find(string str);
-private:
-    vector<int> array;
-}
+    std::set<int> Find(string str);
 
+private:
+    vector<int> index;
+    string data;
+};
+
+set<int> TArray::Find(string str){
+    set<int> result;
+    long long left = 0;
+    long long right = this->index.size();
+
+    vector<const char*> tmp(this->index.size());
+    for (size_t i = 0; i < this->index.size(); ++i){
+        tmp[i] = this->data.data() + (this->index[i] - 1);
+    }
+    int buffer_left, buffer_right;
+    for (size_t i = 0; i < str.size(); ++i){
+        buffer_left = left;
+        buffer_right = right;
+        left = lower_bound(tmp.begin() + buffer_left, tmp.begin() +  buffer_right, str.data()) - tmp.begin();
+        right = upper_bound(tmp.begin() + buffer_right, tmp.begin() + buffer_right, str.data()) - tmp.begin();
+    }
+
+    for (size_t j = left; j < right; ++j) {
+        result.insert(this->index[j]);
+    }
+    return result;
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -33,9 +58,13 @@ int main() {
     TSuffixTree tree(text + "$");
     TArray array(tree);
 
+    set<int> result;
+    uint32_t count = 1;
     while (cin >> pattern){
-
+        result = array.Find(pattern);
+        if (!result.empty())
+            cout << cout << ": " << result << endl;
+        ++count;
     }
-
     return 0;
 }
