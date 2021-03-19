47a32e621422cdd6e28f3abd21c112d9752ab234
nikit34 <permikov134@mail.ru>
2021-03-19 11:01:24 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout.tie(0), cin.tie(0);
+
+    string text, pattern;
+    cin >> text;
+
+    while (cin >> pattern){
+        
+    }
+
+    return 0;
+}
